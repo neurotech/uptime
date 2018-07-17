c7e547f3fe4f078c24ef08fd4f8431ba2a5f113a
Tim Douglas <tim.douglas@phocassoftware.com>
2018-07-17 20:31:12 +1000

    Updated daemon to run as a service

--- daemon/index.js
+++ daemon/index.js
@@ -1,5 +1,5 @@
-const os = require("os");
 const spacetime = require("spacetime");
+const publicIp = require("public-ip");
 const tiny = require("tiny-json-http");
 const config = require("../config");
 const log = require("../log");
@@ -8,23 +8,25 @@ let timestampFormat = "dd MMMM yyyy hh:mm a";
 
 log("info", "Starting status daemon.");
 setInterval(() => {
-  tiny.post(
-    {
-      url: config.statusEndPoint,
-      data: {
-        ip: os.networkInterfaces(),
-        now: spacetime
-          .now()
-          .goto("Australia/Sydney")
-          .format(timestampFormat)
+  publicIp.v4().then(ip => {
+    tiny.post(
+      {
+        url: config.statusEndPoint,
+        data: {
+          ip: ip,
+          now: spacetime
+            .now()
+            .goto("Australia/Sydney")
+            .format(timestampFormat)
+        }
+      },
+      (err, res) => {
+        if (err) {
+          log("error", err);
+        } else {
+          log("success", "Successfully posted uptime.");
+        }
       }
-    },
-    (err, res) => {
-      if (err) {
-        log("error", err);
-      } else {
-        log("success", "Successfully posted uptime.");
-      }
-    }
-  );
+    );
+  });
 }, 1000 * 90);
